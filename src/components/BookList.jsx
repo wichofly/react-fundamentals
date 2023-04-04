bbfa97005509e9d05d884e12ce2ee7eabfde732c
Mauricio Erazo <antonierazo13@hotmail.com>
2023-04-04 16:39:19 +0200

    refactor: use map method

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,52 +1,39 @@
 import Book from './Book';
 
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL900_SR900,600_.jpg',
-  title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
-  author: 'Dav Pilkey',
-};
-
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/714Da0RQA5L._AC_UL900_SR900,600_.jpg',
-  title:
-    'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-  author: ' James Clear',
-};
-
-const thirdBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL900_SR900,600_.jpg',
-  title: 'How to Catch the Easter Bunny',
-  author: ' Adam Wallace',
-};
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL900_SR900,600_.jpg',
+    title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
+    author: 'Dav Pilkey',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/714Da0RQA5L._AC_UL900_SR900,600_.jpg',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: ' James Clear',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL900_SR900,600_.jpg',
+    title: 'How to Catch the Easter Bunny',
+    author: ' Adam Wallace',
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          enim voluptas soluta placeat impedit ipsum laudantium eaque! Optio
-          dolore libero fuga. Ipsa quia animi, architecto pariatur in
-          praesentium incidunt eligendi.
-        </p>
-        <button>Open</button>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+      {books.map((book) => {
+        return <Book img={book.img} title={book.title} author={book.author} />;
+      })}
     </section>
   );
 }
 
 export default BookList;
+
+/*
+  - Made the books array variable to have in items every single book.
+  - Using the map() method, it helps to go through each item and get the information we want. 
+    It simplifies the code to not have so many elements for a component. 
+    In this case after having 3 <Book /> components, now there is only one. 
+*/
