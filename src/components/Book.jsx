d678b415befb41bf00c8e3ce80e5a009fa1392fd
Mauricio Erazo <antonierazo13@hotmail.com>
2023-04-04 12:13:59 +0200

    refactor: make more dinamical with { }

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,17 +2,21 @@ const inlineHeadingStyle = {
   color: '#617d98',
   fontSize: '0.75rem',
   marginTop: '0.5rem',
+  letterSpacing: '2px',
 };
 
 function Book() {
+  const title = 'Dog Man: Twenty Thousand Fleas Under the Sea';
+  const author = 'Dav Pilkey';
+
   return (
     <article className="book">
       <img
         src="https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL900_SR900,600_.jpg"
         alt="Dog man"
       ></img>
-      <h3>Dog Man: Twenty Thousand Fleas Under the Sea</h3>
-      <h3 style={inlineHeadingStyle}>Dav Pilkey</h3>
+      <h3>{title}</h3>
+      <h3 style={inlineHeadingStyle}>{author.toUpperCase()}</h3>
     </article>
   );
 }
