206cd3f64f9c92dec4640abd1236dd73ff1d1473
Mauricio Erazo <antonierazo13@hotmail.com>
2023-04-05 20:54:38 +0200

    refactor: make a new folder for images

--- src/components/booksData.js
+++ src/components/booksData.js
@@ -1,19 +1,23 @@
+import img1 from '../images/book-1.jpg';
+import img2 from '../images/book-2.jpg';
+import img3 from '../images/book-3.jpg';
+
 export const books = [
   {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL900_SR900,600_.jpg',
+    img: img1,
     title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
     author: 'Dav Pilkey',
     id: 1,
   },
   {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/714Da0RQA5L._AC_UL900_SR900,600_.jpg',
+    img: img2,
     title:
       'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
     author: ' James Clear',
     id: 2,
   },
   {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL900_SR900,600_.jpg',
+    img: img3,
     title: 'How to Catch the Easter Bunny',
     author: ' Adam Wallace',
     id: 3,
