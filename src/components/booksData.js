84b17f7e7bb06abd973d21a4f716235e11c7d045
Mauricio Erazo <antonierazo13@hotmail.com>
2023-04-06 17:12:56 +0200

    refactor: add more books in to the data

--- src/components/booksData.js
+++ src/components/booksData.js
@@ -1,6 +1,9 @@
 import img1 from '../images/book-1.jpg';
 import img2 from '../images/book-2.jpg';
 import img3 from '../images/book-3.jpg';
+import img4 from '../images/book-4.jpg';
+import img5 from '../images/book-5.jpg';
+import img6 from '../images/book-6.jpg';
 
 export const books = [
   {
@@ -13,14 +16,32 @@ export const books = [
     img: img2,
     title:
       'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-    author: ' James Clear',
+    author: 'James Clear',
     id: 2,
   },
   {
     img: img3,
     title: 'How to Catch the Easter Bunny',
-    author: ' Adam Wallace',
+    author: 'Adam Wallace',
     id: 3,
+  },
+  {
+    img: img4,
+    title: 'It Starts with Us',
+    author: 'Collen Hoover',
+    id: 3,
+  },
+  {
+    img: img5,
+    title: 'The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma',
+    author: 'Bessel van der Kolk',
+    id: 3,
+  },
+  {
+    img: img6,
+    title: 'Outlive: The Science and Art of Longevity',
+    author: 'Peter Attia',
+    id: 6,
   },
 ];
 
